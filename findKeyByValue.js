0149214e49188e6804b340cb6e22e79ba7306c12
Kennie <Kenneth Chan>
2022-11-17 09:38:14 -0500

    compare key values

---
+++ findKeyByValue.js
@@ -0,0 +1,23 @@
+const assertEqual = function (actual, expected) {
+actual === expected ? console.log(`✅✅✅ Assertion Passed: ${actual} ===  ${expected}`) : console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const findKeyByValue = function (object, value) {
+  let objList = Object.keys(object);
+  for (let key of objList) {
+    if (value === object[key]) {
+      return key
+    }
+  }
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire"
+};
+
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi")
