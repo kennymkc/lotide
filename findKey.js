f9abb1e9ad3e6d63683fe48e762240047aead87b
Kennie <Kenneth Chan>
2022-11-17 21:22:45 -0500

    Find key in objects

---
+++ findKey.js
@@ -0,0 +1,20 @@
+const assertEqual = function (actual, expected) {
+  actual === expected ? console.log(`✅✅✅ Assertion Passed: ${actual} ===  ${expected}`) : console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const findKey = function (object, callback) {
+  for (let obj in object) {
+    if (callback(object[obj])) {
+      return obj;
+    }
+  }
+};
+
+console.log(assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri": { stars: 3 },
+  "noma": { stars: 2 },
+  "elBulli": { stars: 3 },
+  "Ora": { stars: 2 },
+  "Akelarre": { stars: 3 }
+}, x => x.stars === 2), 'noma')) // => "noma"
