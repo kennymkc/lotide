c365cb9bcfacff32eac09d6e8744f9c90ce7de0f
Kennie <Kenneth Chan>
2022-11-21 14:57:34 -0500

    refactored code

--- meow.js
+++
@@ -1,7 +0,0 @@
-var myObjected = {
-  'key-1': 42,
-  keyB: 'value B',
-  'keyC': [1, 2, 3]
-};
-
-console.log(myObjected['key-1'])
--- middle.js
+++ middle.js
@@ -1,36 +1,14 @@
-const eqArrays = function (arr1, arr2) {
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function (arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`✅✅✅ Assertion Passed: Match.`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: Does not match.`);
-  }
-};
-
 const middle = function (array) {
   let ans = [];
   if (array.length <= 2) {
     return [];
   }
   if (array.length % 2 === 0) {
-    ans.push([array[Math.floor((array.length - 1) / 2)]]), ans.push([array[Math.floor(array.length / 2)]]);
+    ans.push(array[Math.floor((array.length - 1) / 2)]), ans.push(array[Math.floor(array.length / 2)]);
   } else {
     ans.push(array[Math.floor(array.length / 2)]);
   }
   return ans;
 };
 
-console.log(middle([1])) // => []
-console.log(middle([1, 2])) // => []
-console.log(middle([1, 2, 3])) // => [2]
-console.log(middle([1, 2, 3, 4, 5])) // => [3]
-console.log(middle([1, 2, 3, 4])) // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
+module.exports = middle;
